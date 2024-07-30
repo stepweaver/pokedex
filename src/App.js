0042d80d8f8bd6430e88e7fc7fd06d68d9e9e606
stepweaver <weaverst@mail.gvsu.edu>
2024-07-30 15:07:58 -0400

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Components from './Components/Components';
+import Parse from 'parse';
 
-function App() {
+Parse.initialize(
+  process.env.REACT_APP_APPLICATION_ID,
+  process.env.REACT_APP_API_KEY
+);
+Parse.serverURL = process.env.REACT_APP_SERVER_URL;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Components />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Components.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Components = () => {
+  return (
+    <div>
+      <h1>Components</h1>
+    </div>
+  );
+};
+
+export default Components;
